84a3712a928d4b6799fcb2522af288e3ff57c1bc
Eman Furrukh <emanfurrukh09@gmail.com>
2024-07-13 14:23:42 +0500

    Create task1.cpp

---
+++ Lab Tasks/Lab9/task1.cpp
@@ -0,0 +1,5 @@
+#include <iostream> 
+using namespace std;
+int main()
+{ cout<<"Ibn \'Umar and Abu Hurairah (may Allah be pleased with them) reported:\n We heard the Messenger of Allah saying (while delivering Khutbah on his wooden pulpit),\n \"Either some people stop neglecting the Friday prayers, or Allah will seal their hearts and they will be among the heedless.\" ";
+}

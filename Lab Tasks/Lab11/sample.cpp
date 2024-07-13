1ff6317624ee645d75c814d2e72ed9d8412a5ab1
Eman Furrukh <emanfurrukh09@gmail.com>
2024-07-13 14:26:54 +0500

    Create sample.cpp

---
+++ Lab Tasks/Lab11/sample.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int u, i;
+  for (u = 1; u <5; u++)
+  {
+     for (i =1; i <=u; i++)
+     { 
+        cout<<i<<"\t";
+     }
+     cout<<endl;    
+  }
+  return 0;
+}
+

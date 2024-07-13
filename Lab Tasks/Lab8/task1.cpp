d035c5697ce4aa15be5db4e1685619371da81060
Eman Furrukh <emanfurrukh09@gmail.com>
2024-07-13 14:22:04 +0500

    Create task1.cpp

---
+++ Lab Tasks/Lab8/task1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main()
+{ cout<< "Narrated Abu Sufyan:\n"<<" That Heraclius sent for him and said, \"What did He, i.e. the Prophet (PBUH) order you?\" I replied, \"He (PBUH) orders us to offer prayers, to give alms, to be chaste and to keep good relations with our relatives.\" " ;
+ return 0;
+}

2959782b30a4a08834855988af0f8b9a4dc3fb91
Eman Furrukh <emanfurrukh09@gmail.com>
2024-07-13 14:25:15 +0500

    Create task1.cpp

---
+++ Lab Tasks/Lab10/task1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout<<"The Prophet (SallAllahu Alayhi Wa Sallam) said:\n \"Lying is wrong, except in three things: the lie of a man to his wife to make her content with him; a lie in war,for war is deception ; or a lit to settle trouble between people.\" \n [Ahmed,6,459. H]";
+   return 0;
+}
